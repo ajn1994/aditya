cceab699c420a56fcf46ef7b831e66d816d032f2
justdial <aditya.nandedkar@justdial.com>
2017-12-19 15:26:37 +0530

    new

---
+++ load.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#this script for current cpu load for 1 min and 5 min
+
+
+load1=`uptime | sed 's/,//g' | awk '{ print $10 }' | cut -d'.' -f1`
+load2=`uptime | sed 's/,//g' | awk '{ print $11 }' | cut -d'.' -f1`
+
+if [ $load1 -ge 16 ] || [ $load2 -ge 16 ]; then
+   load1=`uptime | sed 's/,//g' | awk '{ print $10 }'`
+   load2=`uptime | sed 's/,//g' | awk '{ print $11 }'`
+   echo "WARNING - load average: $load1 $load2"
+   exit 1
+
+elif [ $load1 -ge 18 ] || [ $load2 -ge 18 ]; then
+   load1=`uptime | sed 's/,//g' | awk '{ print $10 }'`
+   load2=`uptime | sed 's/,//g' | awk '{ print $11 }'`
+   echo "CRITICAL - load average: $load1 $load2"
+   exit 2
+
+else
+   load1=`uptime | sed 's/,//g' | awk '{ print $10 }'`
+   load2=`uptime | sed 's/,//g' | awk '{ print $11 }'`
+   echo "OK - load average: $load1 $load2"
+   exit 0
+fi
+
